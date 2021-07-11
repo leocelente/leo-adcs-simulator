7fd7ca952038c063bb1006e2bd5d575bfbc87af0
Leonardo Celente <leocelente@gmail.com>
2021-07-11 03:18:46 -0300

    Missing implementation comments

--- DCM.py
+++ DCM.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+
 def fromQuaternion(quaternion):
+    # TODO: implement
     return np.identity(3)
 
 
 def fromEulerAngle(euler):
+    # TODO: implement
     return np.identity(3)
