1e2794e0974cd50de66c3fcb83935a2dae0ec619
Leonardo Celente <leocelente@gmail.com>
2021-07-13 04:03:55 -0300

    Fixed state shape bug

--- Integrator.py
+++ Integrator.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+import numpy as np
+
 
 def RK4(model, state_prev, t_now: float, t_step: float):
+    state_prev = np.ravel(state_prev)
     k1 = model(t_now, state_prev)
     k2 = model(t_now+t_step/2, state_prev + k1 * t_step/2)
     k3 = model(t_now+t_step/2, state_prev + k2 * t_step/2)
     k4 = model(t_now+t_step, state_prev + k3 * t_step)
     k = (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-    state_next = state_prev + (k * t_step)
+    delta_state = (k * t_step)
+    state_next = state_prev + delta_state
     return state_next
