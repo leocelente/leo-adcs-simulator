6d279ff32c9779669950bceb71d0157811c44ba6
Leonardo Celente <leocelente@gmail.com>
2021-07-14 00:15:45 -0300

    Simpler data capture

--- main.py
+++ main.py
@@ -5,17 +5,15 @@ from matplotlib import pyplot as plt
 from Earth import R
 
 [q0, q1, q2, q3] = np.ravel(q0123_0.T).tolist()
-state = np.matrix([x0, y0, z0, x_dot0, y_dot0, z_dot0,
-                  q0, q1, q2, q3, p0, q0, r0]).T
-number_of_orbits = 1.
-tfinal = period*number_of_orbits
-timestep = 1.0
-t_out = np.linspace(0.0, tfinal, num=int(tfinal))
+state = np.array([[x0, y0, z0, x_dot0, y_dot0, z_dot0,
+                  q0, q1, q2, q3, p0, q0, r0]], dtype=float).T
 
+number_of_orbits: float = 1
+tfinal: float = period*number_of_orbits
+timestep: float = 1.0
+t_out = np.arange(0.0, tfinal, timestep, dtype=float)
 
-xs = []
-ys = []
-zs = []
+position: list[float] = [[], [], []]
 
 print("Start of Simulation")
 for i in range(len(t_out)):
@@ -23,22 +21,22 @@ for i in range(len(t_out)):
         print("T=", i)
 
     state = RK4(Satellite.Model, state, t_out[i], timestep)
-
-    xs.append(np.ravel(state)[0]*1e-3)
-    ys.append(np.ravel(state)[1]*1e-3)
-    zs.append(np.ravel(state)[2]*1e-3)
+    position[0].append(state[0, 0]*1e-3)
+    position[1].append(state[1, 0]*1e-3)
+    position[2].append(state[2, 0]*1e-3)
 
 
 print("End of Simulation")
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.plot3D(xs, ys, zs, 'red')
+ax.plot3D(position[0], position[1],
+          position[2], 'red')
 
-u, v = np.mgrid[0:2*np.pi:100j, 0:np.pi:50j]
+u, v = np.mgrid[0:2*np.pi:10j, 0:np.pi:20j]
 x = R*1e-3*np.cos(u)*np.sin(v)
 y = R*1e-3*np.sin(u)*np.sin(v)
 z = R*1e-3*np.cos(v)
-ax.plot_surface(x, y, z, color="blue", alpha=0.8)
+ax.plot_wireframe(x, y, z, color="lightblue", alpha=0.8)
 # plt.plot(t_out, xs, color="red")
 # plt.plot(t_out, ys, color="green")
 # plt.plot(t_out, zs, color="blue")
