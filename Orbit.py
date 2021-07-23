b55ff197aa69741c7e53d4414c872d05eea31a40
Leonardo Celente <leocelente@gmail.com>
2021-07-23 18:38:32 -0300

    Use numpy trig functions

--- Orbit.py
+++ Orbit.py
@@ -1,17 +1,17 @@
 import numpy as np
 from Earth import R, mu
-from math import pi, sqrt, sin, cos
+from math import pi
 
 altitude: float = 600e3
 position_0 = np.array([[R + altitude, 0, 0]], dtype=float).T
 
 orbit_inclination: float = 54.24 * (pi/180)
 semi_major: float = np.linalg.norm(position_0)
-circular_vel: float = sqrt(mu/semi_major)
-period: float = 2*pi/sqrt(mu)*semi_major**(3/2)
+circular_vel: float = np.sqrt(mu/semi_major)
+period: float = 2*pi/np.sqrt(mu)*semi_major**(3/2)
 
-vel_y_0: float = circular_vel * cos(orbit_inclination)
-vel_z_0: float = circular_vel * sin(orbit_inclination)
+vel_y_0: float = circular_vel * np.cos(orbit_inclination)
+vel_z_0: float = circular_vel * np.sin(orbit_inclination)
 velocity_0 = np.array([[0, vel_y_0, vel_z_0]], dtype=float).T
 
 attitude_0 = np.array([[1, 0, 0, 0]], dtype=float).T
